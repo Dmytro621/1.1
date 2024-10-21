128d437b180469a79291902ee8949bb97d71163c
Dmytro621 <156955449+Dmytro621@users.noreply.github.com>
2024-10-21 18:09:38 +0200

    homework

---
+++ index.js
@@ -0,0 +1,18 @@
+let string = `Text`;
+
+let number = 123
+
+let booleans = "it`s not true"
+
+let nothing = undefined
+
+let obgect = null
+
+let func = 'function'
+
+console.log(typeof string)
+console.log(number)
+console.log(Boolean(booleans))
+console.log(nothing)
+console.log(typeof obgect)
+console.log(func)
